fe7d46d799a1dfb71b0131cd02418262e5cb54f2
Gloriana Mejias S <glorianamejias5@gmail.com>
2026-02-16 22:54:34 -0600

    Veterinario.java

---
+++ Veterinario.java
@@ -0,0 +1,24 @@
+//Atributos
+private String nombre;
+private String especialidad;
+private String telefono;
+
+// Metodo constructor
+Public veterinario(){
+  this.nombre="";
+  this.especialidad="";
+  this.telefono="";
+}
+
+// Metodos 
+public void leerDatos(){
+  nombre=JOptionPane.showInputDialog(null,"Digite el nombre del medico veterinario: ");
+  especialidad=JOptionPane.showInputDialog(null,"Digite la especialidad del medico veterinario: ");
+  telefono=JOptionPane.showInputDialog(null,"Digite el numero de telefono del medico veterinario: ");
+}
+
+public void mostrarDatos(){
+  JOptionPane.showMessageDialog(null, "nombre: "+ nombre +"\n especialidad: " + especialidad + "\n telefono: " + telefono);
+}
+
+
