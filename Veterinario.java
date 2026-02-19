e40078124f1aa3f0cb4185343ef4a2e39a78ff97
Gloriana M. Salazar <glorianamejias5@gmail.com>
2026-02-19 09:51:15 -0600

    Update Veterinario.java

--- Veterinario.java
+++ Veterinario.java
@@ -2,12 +2,14 @@
 private String nombre;
 private String especialidad;
 private String telefono;
+private String codVeterinario;
 
 // Metodo constructor
 Public veterinario(){
   this.nombre="";
   this.especialidad="";
   this.telefono="";
+  this.codVeterinario="";
 }
 
 // Metodos 
@@ -15,12 +17,14 @@ public void leerDatos(){
   nombre=JOptionPane.showInputDialog(null,"Digite el nombre del medico veterinario: ");
   especialidad=JOptionPane.showInputDialog(null,"Digite la especialidad del medico veterinario: ");
   telefono=JOptionPane.showInputDialog(null,"Digite el numero de telefono del medico veterinario: ");
+  codVeterinario=JOptionPane.showInputDialog(null,"Digite el codigo del medico veterinario: ");
 }
 
 public void mostrarDatos(){
   JOptionPane.showMessageDialog(null, "nombre: "+ nombre +
     "\n especialidad: " + especialidad +
-    "\n telefono: " + telefono);
+    "\n telefono: " + telefono +
+    "\n codVeterinario: " + codVeterinario);
 }
 
 
