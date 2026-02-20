f30e98dd1a55da2fd20552a28bca2645acacaf88
Gloriana M. Salazar <glorianamejias5@gmail.com>
2026-02-20 15:05:29 -0600

    Update Servicios.java

--- Servicios.java
+++ Servicios.java
@@ -4,10 +4,11 @@ private double costo;
 private String descripcion;
 
 //Constructor 
-public Servicios
+public Servicios(){
   this.nombreServicio="";
   this.costo=0.00;
   this.descripcion="";
+}
 
 //Getter 
 
