8ec795a352db7073079f5d3b94941f42b47424e6
Gloriana M. Salazar <glorianamejias5@gmail.com>
2026-02-19 19:35:43 -0600

    Create Servicios.java

---
+++ Servicios.java
@@ -0,0 +1,14 @@
+//Atributos
+private String nombreServicio;
+private double costo;
+private String descripcion;
+
+//Constructor 
+public Servicios
+  this.nombreServicio="";
+  this.costo=0.00;
+  this.descripcion="";
+
+//Getter 
+
+    
