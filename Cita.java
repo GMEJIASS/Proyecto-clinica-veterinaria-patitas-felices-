aa2d65c43adcabcce30143112bdf94340c8c9dbb
croman25 <croman40825@ufide.ac.cr>
2026-03-02 21:58:32 -0600

    Update Cita.java

--- Cita.java
+++ Cita.java
@@ -1,1 +1,16 @@
+package proyectoveterinaria;
 
+public class Cita {
+
+    Mascota mascota;
+    Servicios servicio;
+    String fecha;
+    double costo;
+
+    public Cita(Mascota mascota, Servicios servicio, String fecha, double costo) {
+        this.mascota = mascota;
+        this.servicio = servicio;
+        this.fecha = fecha;
+        this.costo = costo;
+    }
+}
