da4ccae91b7721720b1be318a91fc7dad7355105
croman25 <croman40825@ufide.ac.cr>
2026-03-06 16:14:58 -0600

    Update Vacuna.java

--- Vacuna.java
+++ Vacuna.java
@@ -1,2 +1,44 @@
-hhhhhhhhhhhh
-  llllllll
+package vacuna;
+package proyectoveterinaria;
+/**
+ *
+ * @author carlosroman
+ */
+public class Vacuna {
+
+    String nombreVacuna;
+    String descripcion;
+    double costo;
+
+    public Vacuna(String nombreVacuna, String descripcion, double costo) {
+        this.nombreVacuna = nombreVacuna;
+        this.descripcion = descripcion;
+        this.costo = costo;
+    }
+
+    public String getNombreVacuna() {
+        return nombreVacuna;
+    }
+
+    public void setNombreVacuna(String nombreVacuna) {
+        this.nombreVacuna = nombreVacuna;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+    
+}
+
